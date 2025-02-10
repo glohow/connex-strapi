37803642b2d007831b22ac7829e939be758dd339
Hudson <hudson.tran@glohow.com>
2025-02-10 15:38:34 +0700

    rewite vite config

---
+++ strapi-cms/src/admin/vite.config.ts
@@ -0,0 +1,15 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+      fs: {   
+        allow: [
+          '/opt/node_modules',// this is the abs path OUTSIDE the project root causing the Vite error
+          '/opt/app',
+        ],
+      }
+    }
+  });
+};
