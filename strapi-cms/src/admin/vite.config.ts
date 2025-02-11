f680a65b4c6da84547f5a74707d1b4e1a3562467
Hudson <hudson.tran@glohow.com>
2025-02-11 09:57:08 +0700

    rollback hmr vite

--- strapi-cms/src/admin/vite.config.ts
+++ strapi-cms/src/admin/vite.config.ts
@@ -3,17 +3,13 @@ import { mergeConfig, type UserConfig } from "vite";
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
-    server: {
-      fs: {
-        allow: [
-          "/opt/node_modules", // this is the abs path OUTSIDE the project root causing the Vite error
-          "/opt/app",
-        ],
-      },
-      // hmr: {
-      //   host: "glohow-vn-web-dev.glohow.com",
-      //   port: 5173,
-      // },
-    },
+    // server: {
+    //   fs: {
+    //     allow: [
+    //       "/opt/node_modules", // this is the abs path OUTSIDE the project root causing the Vite error
+    //       "/opt/app",
+    //     ],
+    //   },
+    // },
   });
 };
