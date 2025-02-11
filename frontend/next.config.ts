dd024294c322a97f069a92cde8a3c6952fb0c555
Hudson <hudson.tran@glohow.com>
2025-02-12 01:01:48 +0700

    add production base path - frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next"
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	basePath,
 	images: {
 		remotePatterns: [
 			{
