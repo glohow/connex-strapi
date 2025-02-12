97e18c90fbcb40092ddc07a8fc65b546135abffa
Hudson <hudson.tran@glohow.com>
2025-02-12 10:00:03 +0700

    update asset prefix and env variables

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,20 +1,21 @@
 import type { NextConfig } from "next"
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin"
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const isProd = process.env.NEXT_PUBLIC_NODE_ENV === "production"
+const basePath = isProd ? "/connex" : ""
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
 	/* config options here */
 	basePath,
-	assetPrefix: basePath,
+	assetPrefix: isProd ? `${process.env.NEXT_PUBLIC_SITE_URL}` : "",
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'https',
-				hostname: 'glohow-vn-web-dev.glohow.com',
-			}
+				protocol: "https",
+				hostname: "glohow-vn-web-dev.glohow.com",
+			},
 		],
 		localPatterns: [
 			{
@@ -23,7 +24,7 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
-	output: 'standalone',
+	output: "standalone",
 }
 
 export default withNextIntl(nextConfig)
