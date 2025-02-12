daa6106b84129116fcf05838cb09c5243960a567
Hudson <hudson.tran@glohow.com>
2025-02-12 12:22:39 +0700

    force vite websocket using other port

--- strapi-cms/src/admin/vite.config.ts
+++ strapi-cms/src/admin/vite.config.ts
@@ -4,11 +4,19 @@ export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
     server: {
+      host: "0.0.0.0",
+      port: 5173, // ✅ Keep this for local development
+      strictPort: true,
       fs: {
         allow: [
           "/opt/node_modules", // this is the abs path OUTSIDE the project root causing the Vite error
           "/opt/app",
         ],
+      },
+      hmr: {
+        protocol: 'wss',
+        host: process.env.VITE_WS_HOST || 'cms.connex.glohow.com',
+        port: process.env.VITE_WS_PORT || 1337,  // ✅ Force WebSocket to use port 1337
       },
     },
   });
